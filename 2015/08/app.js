f72811197ab28c564d9a3df08569124c741d3f1c
AJ <aj.nice@daumkakao.com>
2015-06-04 18:10:45 +0900

    click

--- 2015/08/app.js
+++ 2015/08/app.js
@@ -13,22 +13,42 @@ var doms = [];
 for(var i=0; i < cards.length; i++){
 
   doms.push(
-    '<div class="card" data-value="' + cards[i]+ '">'
-    + cards[i] + '</div>'
+    '<div class="card" data-value="' + cards[i]+ '">' +
+    '<div class=""> '+ cards[i]  +' </div>' +
+    '</div>'
   );
 }
 
 $board.html(doms.join(''));
 
 
+
+
 //클릭 이벤트 주기
-var cardStatus = 'closed'; // close, open, ....
+var cardStatus = 'close'; // close, open, ....
+var openedValue = 0;
 
 $board.on('click', '.card', function(event){
 
   //
   var $card = $(event.currentTarget);
 
-  $card.addClass('open');
+  if(cardStatus === 'close'){
+
+    $card.addClass('open');
+    openedValue = $card.data('value');
+    cardStatus = 'open1';
+  }
+  else if(cardStatus === "open1"){
+
+    $card.addClass('open');
+    cardStatus = 'open2';
+
+    setTimeout(function(){
+      $('.card').removeClass('open');
+      cardStatus = 'close';
+
+    }, 1000);
+  }
 
 });
--- 2015/08/util.js
+++ 2015/08/util.js
@@ -2,3 +2,8 @@ function shuffle(o){
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 }
+
+
+
+function closeCard(){
+}
