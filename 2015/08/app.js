51fdf22ab2128eaa0d9b6d7d9e0dddd860365a75
AJ <aj.nice@daumkakao.com>
2015-06-04 17:47:51 +0900

    event

--- 2015/08/app.js
+++ 2015/08/app.js
@@ -12,7 +12,23 @@ console.log(cards)
 var doms = [];
 for(var i=0; i < cards.length; i++){
 
-  doms.push('<div class="card">' + cards[i]+ '</div>');
+  doms.push(
+    '<div class="card" data-value="' + cards[i]+ '">'
+    + cards[i] + '</div>'
+  );
 }
 
 $board.html(doms.join(''));
+
+
+//클릭 이벤트 주기
+var cardStatus = 'closed'; // close, open, ....
+
+$board.on('click', '.card', function(event){
+
+  //
+  var $card = $(event.currentTarget);
+
+  $card.addClass('open');
+
+});
