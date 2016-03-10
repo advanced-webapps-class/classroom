6663a7b4999b36e296b77ff9633d879ed9e280bb
AJ <aj.nice@kakaocorp.com>
2016-03-10 18:09:50 +0900

    02

---
+++ 2016/02/app.js
@@ -0,0 +1,59 @@
+//var str = "hello world!";
+//console.log(str);
+//var nick = 'my nick is \'aji\'';
+//console.log(nick);
+
+// document.getElementById('code').innerHTML =
+//  "<s"+"cript src='./app1.js'></s"+"cript>";
+
+// var head= document.getElementsByTagName('head')[0];
+// var script= document.createElement('script');
+//    script.type= 'text/javascript';
+//    script.src= 'app1.js';
+//    head.appendChild(script);
+
+var man = {
+  name: 'aj',
+  age: 30,
+  hobby: 'bike'
+};
+
+// for(var count=0; count < 5; count++){
+//   console.log(count);
+// }
+
+function sum(x){
+    var y = 1;
+    return x+y; //return 문이 없을경우 undefined반환
+}
+
+// console.log();
+var z = sum(10);
+console.log( z );
+
+
+// function test(){
+//     console.log("test()");
+// }
+//
+// var id = setInterval(test, 1000);
+
+// alert("안녕하세요");
+// var who = prompt("당신은 누구세요?");
+// console.log(who);
+
+// if(man.age > 30){
+//   console.log("old");
+// }
+// else if(man.age > 20){
+//   console.log("young")
+// }
+// else {
+//
+// }
+
+// var manName = 'aj';
+// var manAge = 30;
+//
+// man.age
+// mage.ho
