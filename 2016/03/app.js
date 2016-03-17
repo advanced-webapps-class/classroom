35971715b0d2b9ea8427b0b594077d40c5b23eac
AJ <aj.nice@kakaocorp.com>
2016-03-17 18:15:20 +0900

    03

--- 2016/03/app.js
+++ 2016/03/app.js
@@ -33,4 +33,4 @@ function popup(event){
     console.log(++clickCount);
     console.log(arguments, this ,event ,event.type,event.currentTarget);
 }
-document.body.addEventListener('click', popup);
+document.body.addEventListener('click', popup ) ;
