b89892fcf356c4cd922d43f8cb6740e5ce01b245
AJ <aj.nice@kakaocorp.com>
2016-03-31 16:48:25 +0900

    05-4

--- 2016/05/app.js
+++ 2016/05/app.js
@@ -1,22 +1,29 @@
 console.log('app.js');
 //구구단
 
-var str = '';
-var wrap = document.getElementById('wrap');
-var template = document.getElementById('templateString').innerHTML;
+function initModels(){
 var models = [];
+  for(var i=1; i<=9; i++){
+   // models[i] = i;
+   models[i] = []; //new Array();
+   // str += ("<h2>" + i + "단 시작......</h2>");
+   for(var k=1; k<=9; k++){
+     models[i][k] = i * k;
+    //  str += (i +"x"+ k + "=" + (i * k)) + "<br>";
+   }
+  }
+  return models;
+}
+function renderView(models){
+  var template = document.getElementById('templateString').innerHTML;
+  var wrap = document.getElementById('wrap');
+  var html = tmpl(template, {list: models});
 
-for(var i=1; i<=9; i++){
- // models[i] = i;
- models[i] = []; //new Array();
- // str += ("<h2>" + i + "단 시작......</h2>");
- for(var k=1; k<=9; k++){
-   models[i][k] = i * k;
-  //  str += (i +"x"+ k + "=" + (i * k)) + "<br>";
- }
+  wrap.innerHTML = html;
 }
 
-  var html = tmpl(template, {list: models});
-  wrap.innerHTML = html;
+var models = initModels();
+renderView(models);
+
 
 // console.log(models)
