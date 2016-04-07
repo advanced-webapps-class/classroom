e47a115e14976230a29f282851174d7a6142593b
AJ <aj.nice@kakaocorp.com>
2016-04-07 17:42:17 +0900

    edit

--- 2016/06/openapi.js
+++ 2016/06/openapi.js
@@ -2,3 +2,16 @@ console.log('openapi.js')
 
 
 var apikey = '2c5481187fc5fd8012a831b26eb89960';
+
+var q = '제주대';
+var apiurl = 'https://apis.daum.net/search/vclip?q='+ q +'&apikey='+apikey+
+ '&output=json';
+
+
+ console.log(apiurl)
+
+ getJSON(apiurl, function(res){
+   console.log(res);
+
+
+ });
