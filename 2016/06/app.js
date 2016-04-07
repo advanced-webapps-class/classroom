cbc9cd29abc7feea8ef23cb8fd840b71a9bb1c5e
AJ <aj.nice@kakaocorp.com>
2016-04-07 16:31:26 +0900

    edit

--- 2016/06/app.js
+++ 2016/06/app.js
@@ -1,10 +1,15 @@
 
 console.log('app.js');
 
-var apiurl = "http://m.movie.daum.net/data/movie/movie_info/box_office.json?country=KR&startDate=20150101&endDate=20150401&pageSize=10&callback=done&pageNo=";
+var apiurl = "http://m.movie.daum.net/data/movie/movie_info/box_office.json?country=KR&startDate=20160101&endDate=20160401&pageSize=10&pageNo=1";
 
 
 
-ajax(apiurl, function(responseText){
-    console.log(responseText);
+// 서로  도메인이  달라서  호출할수 없음
+// ajax(apiurl, function(responseText){
+//     console.log(responseText);
+// });
+
+getJSON(apiurl, function(data){
+  console.log(data);
 });
