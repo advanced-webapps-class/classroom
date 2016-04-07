e1edd7c1c8d6fa59706834c82592d82643ffc2c5
AJ <aj.nice@kakaocorp.com>
2016-04-07 16:52:17 +0900

    edit

--- 2016/06/app.js
+++ 2016/06/app.js
@@ -1,15 +1,37 @@
 
 console.log('app.js');
 
-var apiurl = "http://m.movie.daum.net/data/movie/movie_info/box_office.json?country=KR&startDate=20160101&endDate=20160401&pageSize=10&pageNo=1";
+var page = 1;
+var apiurl = "http://m.movie.daum.net/data/movie/movie_info/box_office.json?country=KR&startDate=20150101&endDate=20160401&pageSize=2&pageNo=1";
 
+var wrap = document.getElementById('wrap');
+var template = document.getElementById('movieListTemplate').innerHTML;
 
 
 // 서로  도메인이  달라서  호출할수 없음
 // ajax(apiurl, function(responseText){
 //     console.log(responseText);
 // });
+// var data = [
+//   {
+//     title: '이것은 제목입니다',
+//     img: 'http://icon.daumcdn.net/w/icon/1312/19/152729032.png',
+//     starRating: 3,
+//     link: 'http://daum.net',
+//   }
+// ];
+//
+function render(wrap, template, data){
 
-getJSON(apiurl, function(data){
-  console.log(data);
+  var html = tmpl(template, {list: data});
+  wrap.innerHTML = html;
+}
+
+
+
+getJSON(apiurl, function(res){
+  console.log(res);
+
+  render( wrap, template, res.data);
+
 });
