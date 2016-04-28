11f1916648be7b31509bd1279386d67f235b3c6b
AJ <aj.nice@kakaocorp.com>
2016-04-28 18:19:29 +0900

    08

---
+++ 2016/08/game.js
@@ -0,0 +1,41 @@
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
+function printCards(){
+  cards.forEach(function(card){
+    $wrap.append('<div class="card close" num="'+card+'">\
+    <div class="front">  </div> \
+    <div class="back">'+ card +'</div></div>');
+  });
+}
+
+function addEvent(){
+
+  $wrap.on('click', '.card', function(event){
+
+    var $target = $(event.currentTarget);
+    var num = $target.attr('num');
+
+    console.log('click card!', num)
+
+
+
+});
+}
+
+var cards = [1,2,3,4, 1,2,3,4];
+var $wrap = $('.wrap');
+
+//init
+shuffle(cards);
+console.log(cards)
+printCards();
+addEvent();
