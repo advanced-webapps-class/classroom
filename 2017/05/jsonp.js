32a017f70821703eed0b7619009a59b13a86b684
AJ <aj.nice@kakaocorp.com>
2017-10-13 17:13:57 +0900

    edit

--- 2017/05/jsonp.js
+++ 2017/05/jsonp.js
@@ -1,9 +1,19 @@
 
 
 function done(data){
-  console.log(data)
+  console.log('done:', data)
 }
 
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click', load);
+
+function load(){
+  var head = document.getElementsByTagName("head")[0];
+  var script = document.createElement('script');
+  script.src = 'http://1boon.kakao.com/issue.json?callback=done';
+  head.appendChild(script);
+}
 // done(
 //   {
 //     "a":1,
