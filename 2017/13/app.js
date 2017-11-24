9c52821dd767c65f43e2265cd4b2dc4db78f01aa
AJ <aj.nice@kakaocorp.com>
2017-11-24 17:02:29 +0900

    edit

--- 2017/13/app.js
+++ 2017/13/app.js
@@ -3,8 +3,50 @@
 $('#read').on('click', function(event){
   console.log('read');
   
-  $.get('http://localhost:3000/posts', function(data){
-    
+  $.get('http://localhost:3000/posts', function(data){    
     console.log(data);
   })
+});
+
+$('#create').on('click', function(event){
+  console.log('create');
+  
+  $.post('http://localhost:3000/posts', {title: '하하하하'},  function(data){    
+    console.log(data);
+  })
+});
+
+$('#remove').on('click', function(event){
+  console.log('remove');
+  
+  $.ajax({
+    url: 'http://localhost:3000/posts/1',
+    method: 'DELETE',
+    complete: function(data){
+        console.log(data);
+
+    }
+    
+  })
+});
+
+
+$('#update').on('click', function(event){
+  console.log('update');
+  
+  // $.put('http://localhost:3000/posts/1', {title: '수정입니다' + (new Date().getTime() ) },  function(data){    
+  //   console.log(data);
+  // })
+  
+  $.ajax({
+    url: 'http://localhost:3000/posts/1',
+    method: 'PUT',
+    data: {title: '수정입니다' + (new Date().getTime() ) },
+    complete: function(data){
+        console.log(data);
+
+    }
+    
+  })
+  
 });
