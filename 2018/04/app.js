049aaa77b6bad350c88ea753cb2c702c7908c97b
AJ <aj.nice@kakaocorp.com>
2018-09-21 17:31:26 +0900

    04

--- 2018/04/app.js
+++ 2018/04/app.js
@@ -1,12 +1,13 @@
 var wrap = document.querySelector('.wrap');
 var str = '';
 
-for(let i = 1; i <= 4; i++) {
-  for(let j = 1; j <= 4; j++) {
+for(var i = 1; i <= 4; i++) {
+  for(var j = 1; j <= 4; j++) {
       // let block = document.createElement('div');
       // block.className = (i + j) % 2 == 1 ? 'black' : 'white';
       // wrap.appendChild(block);
-      str += `<div class='${(i + j) % 2 == 1 ? 'b' : 'w'}'></div>`  
+      var className = (i + j) % 2 == 1 ? 'b' : 'w'; 
+      str += '<div class="'+ className +'"></div>'  
   }
 }
 
@@ -25,13 +26,15 @@ var blocks = document.querySelectorAll('.wrap > div');
 //   })
 
 // });
+
 function select(event) {
   var block = event.currentTarget;
   block.style.backgroundColor = 'yellow';
+  //     // block.className += ' y';
+
 }
 for(var i=0; i < 16; i++) {
   // console.log( blocks[i] );
-
   blocks[i].addEventListener('click', select);
 
 }
