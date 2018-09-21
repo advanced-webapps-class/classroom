94eded9fa7a7d095e1fc9f4967ef9aeff631a726
AJ <aj.nice@kakaocorp.com>
2018-09-21 17:09:27 +0900

    04

--- 2018/04/app.js
+++ 2018/04/app.js
@@ -1,8 +1,14 @@
 var wrap = document.querySelector('.wrap');
-
 var str = '';
-for(var i=0; i < 16; i++){
-  // str = str +  '<div class="b"></div>';
-  str +=  '<div class="b"></div>';
+
+for(let i = 1; i <= 4; i++) {
+  for(let j = 1; j <= 4; j++) {
+      // let block = document.createElement('div');
+      // block.className = (i + j) % 2 == 1 ? 'black' : 'white';
+      // wrap.appendChild(block);
+      str += `<div class='${(i + j) % 2 == 1 ? 'b' : 'w'}'></div>`  
+  }
 }
+
+
 wrap.innerHTML = str;
