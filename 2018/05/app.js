8826653d4f29f9badd8707ea7c48be5a2da6e324
AJ <aj.nice@kakaocorp.com>
2018-09-28 16:55:29 +0900

    edit

--- 2018/05/app.js
+++ 2018/05/app.js
@@ -1,9 +1,23 @@
 console.log('app.js');
 
-fetch('./data.json')
-.then(function(response){
-  response.json().then(function(data){
-    console.log('json data:', data);
-    document.getElementById('name').innerHTML = data.name;
-  });
-});
+function json() {
+  fetch('./data.json')
+    .then(function(response) {
+      response.json().then(function(data) {
+        console.log('json data:', data);
+        document.getElementById('name').innerHTML = data.name;
+      });
+    })
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+}
+
+function jsonp() {
+  var head = document.querySelector('head');
+  var script = document.createElement('script');
+  script.src = 'jsonp.js';
+  head.appendChild(script);
+}
+
+jsonp();
