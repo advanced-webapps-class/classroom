883ac78a3aee96e897ffd925679a12f66c287d78
AJ <aj.nice@kakaocorp.com>
2018-09-28 17:46:28 +0900

    1boon

--- 2018/05/1boon/app.js
+++ 2018/05/1boon/app.js
@@ -2,21 +2,26 @@ console.log('1boon');
 
 var url = 'https://1boon.kakao.com/ch/enter.json?page=1&pagesize=10';
 
+getUrlData(url, print);
 
-getUrlData(url, function(json){
-  console.log( json );
+function print(json) {
+  console.log(json);
 
   var str = '';
 
-  for(var i=0; i < json.data.length; i++){
-
+  for (var i = 0; i < json.data.length; i++) {
     var title = json.data[i].title;
-    var path =json.data[i].path; 
-    str += '<a href="https://1boon.kakao.com/'+path+'">' + title + '</a><br>';
+    var path = json.data[i].path;
+    str +=
+      '<a href="https://1boon.kakao.com/' +
+      path +
+      '" target="_blank">' +
+      title +
+      '</a><br>';
   }
 
   document.getElementById('wrap').innerHTML = str;
-})
+}
 
 
 function getUrlData(url, callback) {
@@ -24,7 +29,7 @@ function getUrlData(url, callback) {
     .then(function(response) {
       response.json().then(function(data) {
         // console.log('json data:', data);
-        callback(data)
+        callback(data);
       });
     })
     .catch(function(err) {
