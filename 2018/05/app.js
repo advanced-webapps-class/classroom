12f3ff59a608b11b8338e4a7eaf3110ad3ff2a49
AJ <aj.nice@kakaocorp.com>
2018-09-28 17:21:00 +0900

    1boon

--- 2018/05/app.js
+++ 2018/05/app.js
@@ -1,17 +1,19 @@
 console.log('app.js');
 
 function json() {
-  fetch('./data.json')
+  var url = 'https://1boon.kakao.com/ch/enter.json?page=1&pagesize=1';
+  fetch(url)
     .then(function(response) {
       response.json().then(function(data) {
         console.log('json data:', data);
-        document.getElementById('name').innerHTML = data.name;
+        // document.getElementById('name').innerHTML = data.name;
       });
     })
     .catch(function(err) {
       console.log('Fetch Error :-S', err);
     });
 }
+json();
 
 function jsonp() {
   var head = document.querySelector('head');
@@ -24,4 +26,4 @@ function callback(data) {
   console.log(data)
   document.getElementById('name').innerHTML = data.name;
 }
-jsonp();
+// jsonp();
