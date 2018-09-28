81ae99c240bbbc96a1301c8bbba1276eafbb0495
AJ <aj.nice@kakaocorp.com>
2018-09-28 17:26:28 +0900

    1boon

--- 2018/05/1boon/app.js
+++ 2018/05/1boon/app.js
@@ -1,1 +1,22 @@
 console.log('1boon');
+
+var url = 'https://1boon.kakao.com/ch/enter.json?page=1&pagesize=10';
+
+
+getUrlData(url, function(data){
+  console.log( data );
+})
+
+
+function getUrlData(url, callback) {
+  fetch(url)
+    .then(function(response) {
+      response.json().then(function(data) {
+        // console.log('json data:', data);
+        callback(data)
+      });
+    })
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+}
