7f04839549c7000471563d2b9628387becc21003
AJ <aj.nice@kakaocorp.com>
2018-10-05 17:00:41 +0900

    edit

--- 2018/06/app.js
+++ 2018/06/app.js
@@ -1,5 +1,17 @@
-console.log('app')
+console.log('app');
 
 var appkey = 'e4d234929bd36175a103f0bd0bca3757';
 var query = '제주대학교';
 var url = `https://dapi.kakao.com/v2/search/web?query=${query}`;
+
+var myHeaders = new Headers();
+myHeaders.append('Authorization', 'KakaoAK e4d234929bd36175a103f0bd0bca3757');
+var options = {
+  headers: myHeaders
+};
+
+fetch(url, options).then(function(response) {
+  response.json().then(function(data) {
+    console.log('json data:', data);
+  });
+});
