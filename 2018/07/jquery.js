5ae9b23cd0ed06f14dac887e23ef19b9e2ba9b9c
AJ <aj.nice@kakaocorp.com>
2018-10-12 16:42:00 +0900

    edit

---
+++ 2018/07/jquery.js
@@ -0,0 +1,16 @@
+// document.querySelector('h1').innerHTML = 'hi';
+
+//  $('h1').fadeOut().delay(1000).fadeIn();
+
+var $div = $('div')
+
+// $div
+//   .css('background-color', 'yellow')
+//   .css('border', '1px solid red');
+
+
+
+// var divs = document.querySelectorAll('div');
+// for(var i=0 ; i < divs.length; i++) {
+//   divs[i].style.backgroundColor = 'red';
+// }
