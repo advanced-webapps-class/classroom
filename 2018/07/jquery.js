797991035795d4acd851978fbf06951bbdc32748
AJ <aj.nice@kakaocorp.com>
2018-10-12 17:24:03 +0900

    edit

--- 2018/07/jquery.js
+++ 2018/07/jquery.js
@@ -17,18 +17,26 @@
 
 
 
-
 var $btn = $('#btn');
 var $txt = $('#txt');
 var $debug = $('#debug');
 
 $btn.on('click', function(event){
   console.log('click')
-  if($txt.val() === '') {
-    alert('값을 넣어주세요')
-    return;
-  }
 
-  $debug.html($txt.val());
+  $('h1').toggle('fast'); //fadeOut();
+  // if($txt.val() === '') {
+  //   alert('값을 넣어주세요')
+  //   return;
+  // }
+  // $debug.html($txt.val());
 
 });
+
+
+$('.wrap').on('click', 'div', function(event){
+
+  $(event.target).fadeOut();
+  // console.log(event.target);
+
+})
