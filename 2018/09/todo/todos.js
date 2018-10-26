d801df4064fa7577600e1cd0e9a8bf128c7d1ee6
AJ <aj.nice@kakaocorp.com>
2018-10-26 16:47:02 +0900

    todo

--- 2018/09/todo/todos.js
+++ 2018/09/todo/todos.js
@@ -1,1 +1,39 @@
 //start...
+
+//input입력받기 
+
+
+var input = document.getElementById('memo');
+var list = document.getElementById('list');
+var form = document.querySelector('.new-task');
+
+
+function addTodo(event){
+  // if(event.keyCode === 13) {
+  //   var todo = input.value;
+  //   console.log(todo);
+  // }
+  // console.log(event);
+
+
+  var todo = input.value;
+  console.log(todo); 
+  insertTodo(todo);
+
+  input.value = '';
+  event.preventDefault();
+}
+
+// input.addEventListener('keydown', addTodo);
+form.addEventListener('submit', addTodo);
+
+function insertTodo(todo){
+
+  var html = `<li>
+        <button class="delete">×</button>
+        <input type="checkbox" class="toggle-checked">
+        <span class="text">${todo}</span>
+      </li>`;
+    
+  list.innerHTML += html;    
+}
