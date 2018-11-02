29a79064857834c4ef4077a945797a02396a2bca
AJ <aj.nice@kakaocorp.com>
2018-11-02 16:45:23 +0900

    09,10

--- 2018/09/todo/todos.js
+++ 2018/09/todo/todos.js
@@ -1,39 +1,57 @@
-//start...
-
-//input입력받기 
-
-
-var input = document.getElementById('memo');
-var list = document.getElementById('list');
+var input = document.querySelector('#memo');
 var form = document.querySelector('.new-task');
+var todoList = document.querySelector('#todo-list')
 
+function addTodo(event) {   
+    let todo = input.value;
+    if(todo.trim() !== '') {
+        console.log(todo);
+        insertTodo(todo);
+        input.value = "";
+    }
+    
 
-function addTodo(event){
-  // if(event.keyCode === 13) {
-  //   var todo = input.value;
-  //   console.log(todo);
-  // }
-  // console.log(event);
+    // 해당 url로 넘어가지 않게 동작하기
+    // return false;
+    // 혹은 동작하지 않게 하기
+    event.preventDefault();
+}
+function doneTodo(event) {
+    let parent = event.target.parentNode;
+    let todoText = parent.querySelector('span.text');
+    if(event.target.checked) {
+        todoText.classList.add('done');
+    }
+    else {
+        todoText.classList.remove('done');
+    }
+        
+}
 
+function deleteTodo(event) {
+  debugger
+    todoList.removeChild(event.target.parentNode);
+}
 
-  var todo = input.value;
-  console.log(todo); 
-  insertTodo(todo);
-
-  input.value = '';
-  event.preventDefault();
+function insertTodo(todo) {
+    let todoLi = document.createElement('li');
+    todoLi.innerHTML = `
+        <button class="delete">x</button>
+        <input type="checkbox" class="toggle-checked">
+        <span class="text">${todo}</span>
+    `
+    todoLi.querySelector('.delete').addEventListener('click', deleteTodo);
+    console.log(todoLi.querySelector('.toggle-checked'));
+    todoLi.querySelector('.toggle-checked').addEventListener('click', doneTodo);
+    todoList.appendChild(todoLi);
+    // var todoBlock = `
+    //     <li>
+    //         <button class="delete">x</button>
+    //         <input type="checkbox" class="toggle-checked">
+    //         <span class="text">${todo}</span>
+    //     </li>
+    // `
 }
 
 // input.addEventListener('keydown', addTodo);
 form.addEventListener('submit', addTodo);
-
-function insertTodo(todo){
-
-  var html = `<li>
-        <button class="delete">×</button>
-        <input type="checkbox" class="toggle-checked">
-        <span class="text">${todo}</span>
-      </li>`;
-    
-  list.innerHTML += html;    
-}
