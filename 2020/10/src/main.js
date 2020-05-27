ea2081b3c300dd81e17166b2f003c7a3c02b3fb3
AJ <aj.nice@kakaocorp.com>
2020-05-27 14:54:07 +0900

    10

---
+++ 2020/10/src/main.js
@@ -0,0 +1,3 @@
+import sum from './sum';
+
+document.getElementById('debug').innerHTML = sum(10111, 2);
---
+++ 2020/10/webpack.config.js
@@ -0,0 +1,17 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'app.js',
+    path: `${__dirname}/dist`,
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
