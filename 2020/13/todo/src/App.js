7588f721f96aba4773bd2819201ed313a9b22717
AJ <aj.nice@kakaocorp.com>
2020-06-16 16:47:22 +0900

    todo

--- 2020/13/todo/src/App.js
+++ 2020/13/todo/src/App.js
@@ -2,17 +2,42 @@ import React, { useState } from 'react';
 import Header from './Header';
 
 function App() {
-  const [countNumber, setCountNumber] = useState(0);
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
 
-  function addCountNumber() {
-    setCountNumber(countNumber + 1);
+  function handleChange(event) {
+    setTodo(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newTodos = [...todos];
+    newTodos.push(todo);
+    setTodos(newTodos);
+    setTodo('');
   }
 
+  console.log(todos);
+
   return (
-    <div className="App">
-      <Header />
-      <div>{countNumber}</div>
-      <button onClick={addCountNumber}>플러스</button>
+    <div className="container">
+      <header>
+        <Header />
+        <form className="new-task" onSubmit={handleSubmit}>
+          <input
+            name="text"
+            placeholder="할일을 입력하세요"
+            value={todo}
+            onChange={handleChange}
+          />
+        </form>
+      </header>
+      <div id="result">
+        <ul>
+          {todos.map((todo) => {
+            return <li>{todo}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
