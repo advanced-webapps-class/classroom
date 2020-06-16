ef657810f54315aa49a17d160b26d041660b8359
AJ <aj.nice@kakaocorp.com>
2020-06-16 15:57:46 +0900

    count

--- 2020/13/todo/src/App.js
+++ 2020/13/todo/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
-  return <div className="App">Hello23231111</div>;
+  const [countNumber, setCountNumber] = useState(0);
+
+  function addCountNumber() {
+    setCountNumber(countNumber + 1);
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <div>{countNumber}</div>
+      <button onClick={addCountNumber}>플러스</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ 2020/13/todo/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1>Todo</h1>
+    </div>
+  );
+}
+
+export default Header;
