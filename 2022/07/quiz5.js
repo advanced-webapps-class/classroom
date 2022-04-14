62e0216294f56f0b3611049baacece28de6b8fa6
AJ <aj.nice@kakaocorp.com>
2022-04-14 16:21:30 +0900

    edit

--- 2022/07/quiz5.js
+++ 2022/07/quiz5.js
@@ -1,33 +1,43 @@
-const $box = document.getElementById('box');
+let $dragEl = null;
 
-console.log($box.offsetTop);
+// console.log($box.offsetTop);
 
 let offset = { x: 0, y: 0 };
 let isDown = false;
-
-$box.addEventListener('mousedown', (event) => {
-  isDown = true;
-  // console.log($box.offsetLeft);
-  console.log('mousedown');
-  offset.x = $box.offsetLeft - event.clientX;
-  offset.y = $box.offsetTop - event.clientY;
-  // offset.x = event.clientX;
-  // offset.y = event.clientY;
-});
-
-$box.addEventListener('mouseup', (event) => {
-  isDown = false;
-  console.log('mouseup');
-});
 
 document.body.addEventListener('mousemove', (event) => {
   console.log('mousemove');
   if (!isDown) return;
   // console.log(event.clientX, event.clientY);
 
-  $box.style.left = event.clientX + offset.x;
-  $box.style.top = event.clientY + offset.y;
+  $dragEl.style.left = event.clientX + offset.x;
+  $dragEl.style.top = event.clientY + offset.y;
 
   // $box.style.left = event.clientX;
   // $box.style.top = event.clientY;
 });
+
+function startDrag(el) {
+  el.addEventListener('mousedown', (event) => {
+    $dragEl = el;
+    isDown = true;
+    // console.log($box.offsetLeft);
+    console.log('mousedown');
+    offset.x = $dragEl.offsetLeft - event.clientX;
+    offset.y = $dragEl.offsetTop - event.clientY;
+    // offset.x = event.clientX;
+    // offset.y = event.clientY;
+  });
+
+  el.addEventListener('mouseup', (event) => {
+    isDown = false;
+    console.log('mouseup');
+  });
+}
+// -----------------
+
+const $box1 = document.querySelector('.box1');
+const $box2 = document.querySelector('.box2');
+
+startDrag($box1);
+startDrag($box2);
