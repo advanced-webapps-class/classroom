5aecbe3cf95a7f6e0b2076b95bdc2f4b842a3d01
AJ <aj.nice@kakaocorp.com>
2022-04-28 17:38:00 +0900

    edit

--- 2022/09/fetch.js
+++ 2022/09/fetch.js
@@ -4,7 +4,10 @@ function getFetch(url, callback) {
     .then((data) => callback(data));
 }
 
-getFetch('data.json', (data) => console.log(data));
+const url =
+  'https://sports.daum.net/prx/hermes/api/game/list.json?page=1&filter=importance&date=20220428&pageSize=50&detail=true&leagueCode=KL%2CKL2%2CKL_RELEGATION%2CKFA%2CAFCCL%2CEPL%2CBUNDESLIGA%2CPRIMERA%2CSERIEA%2CLIGUE1%2CUEFACL%2CUEFACUP%2CEPL_CUP%2CFACUP%2CCOPADELREY%2CKBO%2CMLB%2CNBA%2CKBL%2CWKBL%2CVL%2CVL_CUP%2CWVL%2CWVL_CUP%2CKLPGA%2CKPGA%2CLPGA%2CPGA%2CLCK';
+
+getFetch(url, (data) => console.log(data));
 
 // const data = fetch('data.json');
 // console.log(data.json);
