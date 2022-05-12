668953326c1542834a983439e79038eb9a5f9612
AJ <aj.nice@kakaocorp.com>
2022-05-12 16:26:25 +0900

    edit

--- 2022/11/app.js
+++ 2022/11/app.js
@@ -1,5 +1,13 @@
 function getFetch(url, callback) {
-  fetch(url)
+  const headers = {
+    Authorization: 'KakaoAK ad1eb3ae6a7731c9d17a6c0f88a5fed6',
+  };
+
+  fetch(url, { headers })
     .then((response) => response.json())
     .then((data) => callback(data));
 }
+
+const url = `https://dapi.kakao.com/v2/search/web?query=이효리`;
+
+getFetch(url, (data) => console.log(data));
