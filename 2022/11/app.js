833badec6385ee663579bf6c7d8dd791bb1d87b9
AJ <aj.nice@kakaocorp.com>
2022-05-12 16:36:23 +0900

    edit

--- 2022/11/app.js
+++ 2022/11/app.js
@@ -1,3 +1,6 @@
+const url = `https://dapi.kakao.com/v2/search/web?query=이효리`;
+const $docs = document.querySelector('#docs');
+
 function getFetch(url, callback) {
   const headers = {
     Authorization: 'KakaoAK ad1eb3ae6a7731c9d17a6c0f88a5fed6',
@@ -8,6 +11,16 @@ function getFetch(url, callback) {
     .then((data) => callback(data));
 }
 
-const url = `https://dapi.kakao.com/v2/search/web?query=이효리`;
+getFetch(url, (data) => {
+  const { documents } = data;
+  // const documents = data.documents;
+  console.log(documents);
 
-getFetch(url, (data) => console.log(data));
+  const docs = documents.map((document) => {
+    // console.log(document);
+    return document.contents;
+  });
+
+  // console.log(docs);
+  $docs.innerHTML = docs.join('<hr>');
+});
