5f9221b1f3510b13d615838ba33cf44af2a238f1
AJ <aj.nice@kakaocorp.com>
2022-05-19 16:23:38 +0900

    edit

---
+++ 2022/12/app.js
@@ -0,0 +1,5 @@
+import sum1 from './sum.js';
+import { sum2 } from './sum.js';
+
+console.log(sum1(1, 2));
+console.log(sum2(2, 2));
---
+++ 2022/12/sum.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+  return a + b;
+}
+export default sum;
+
+export function sum2(a, b) {
+  return a + b;
+}
