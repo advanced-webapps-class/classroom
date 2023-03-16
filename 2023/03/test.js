86124929b1221b3f8ba898d6253285b52ac69c7f
AJ <aj.nice@kakaocorp.com>
2023-03-16 17:02:36 +0900

    03

--- 2023/03/test.js
+++ 2023/03/test.js
@@ -1,4 +1,53 @@
-const a = 'a';
-console.log(a);
-a = '11';
+// const a = 'a';
+// console.log(a);
+// a = '11';
+// console.log(a);
+
+// array create
+// const todos = ['운동'];
+// const todo = '공부';
+// todos.push(todo);
+// console.log(todos);
+
+// // read
+// todos.forEach(function (todo) {
+//   console.log(todo);
+// });
+
+// update
+// const updateTodo = '공부';
+// const updateIndex = todos.findIndex(function (todo) {
+//   return todo === updateTodo;
+// });
+
+// todos[updateIndex] = '게임';
+// console.log(todos);
+
+// const newTodos = todos.map(function (todo) {
+//   if (todo === updateTodo) {
+//     return '게임';
+//   }
+//   return todo;
+// });
+// console.log(newTodos);
+
+//
+
+// var a = 1;
+// var b = a;
+// b = 2;
+// console.log(a, b);
+
+// var a = [1];
+// var b = a;
+// b[0] = [2];
+// console.log(a, b);
+
+// var a = { x: 1 };
+// var b = a;
+// b.x = 2;
+// console.log(a, b);
+
+const a = { a: 1, b: 1 };
+a.c = 1;
 console.log(a);
