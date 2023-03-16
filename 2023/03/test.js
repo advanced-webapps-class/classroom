726232225baa38ed31a9202e52dd51c70bf3e349
AJ <aj.nice@kakaocorp.com>
2023-03-17 08:34:55 +0900

    edit

--- 2023/03/test.js
+++ 2023/03/test.js
@@ -48,6 +48,47 @@
 // b.x = 2;
 // console.log(a, b);
 
-const a = { a: 1, b: 1 };
-a.c = 1;
-console.log(a);
+// const a = { a: 1, b: 1 };
+// a.c = 1;
+// console.log(a);
+
+// function test() {
+//   console.log('test()');
+// }
+
+// var id = setInterval(test, 1000); //1초마다 계속 실행
+
+// clearInterval(id);
+
+// var nick = 'aji'; //전역변수
+
+// function test() {
+//   var age = 30; //지역변수
+//   console.log(nick, age);
+// }
+
+// test();
+// console.log(age);
+
+// var name = 'global';
+// function test() {
+//   console.log(name);
+//   var name = 'local';
+//   console.log(name);
+// }
+// test();
+
+// test2();
+
+// function test2() {
+//   console.log('test');
+// }
+
+let value = undefined;
+let count = 0;
+
+while (value !== null) {
+  value = window.prompt();
+  count += value * 1;
+}
+console.log(count);
