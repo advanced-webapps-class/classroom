7f347864a9617bffad0c7aa3ca50e955a8a7d5ba
AJ <aj.nice@kakaocorp.com>
2023-03-30 18:15:07 +0900

    05

---
+++ 2023/05/test.js
@@ -0,0 +1,32 @@
+console.log('05');
+
+window.addEventListener('click', () => {
+  console.log('window load event1');
+});
+
+window.addEventListener('click', () => {
+  console.log('window load event2');
+});
+
+// const wrap = document.getElementById('wrap');
+// console.log(wrap);
+
+// const item = document.querySelector('#wrap  > .item');
+// console.log(item);
+// const elements = document.querySelectorAll('div');
+// console.log(elements);
+
+// const div = document.createElement('div');
+// div.style.border = '1px solid red';
+// div.innerHTML = '<div>aasssaaaa</div>';
+
+// document.body.innerHTML = ''
+// document.body.appendChild(div);
+
+// const span = document.createElement('span');
+// const textNode = document.createTextNode('hello!');
+// span.appendChild(textNode);
+// document.querySelector('#debug').appendChild(span);
+
+// const text = '<span>hello</span>';
+// document.querySelector('#debug').innerHTML = text;
